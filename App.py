8dff43596c6cc86672bb75242b51864b63fc072b
otman-dev <mouhib_otm@upf.ac.ma>
2023-08-13 01:39:36 +0100

    get_dataset get input from user to dowload dataset

--- App.py
+++ App.py
@@ -143,10 +143,15 @@ def train():
 
 
 def get_dataset():
-    rbflw_api_key = "BtcWZsdvGqmTQ2Sfmgbj"
-    rbflw_workspace = "moho-rahimi-xyr0w"
-    rbflw_project = "wildfire-ryisc"
-    rbflw_download = "yolov8"
+    #rbflw_api_key = "BtcWZsdvGqmTQ2Sfmgbj"
+    #rbflw_workspace = "moho-rahimi-xyr0w"
+    #rbflw_project = "wildfire-ryisc"
+    #rbflw_download = "yolov8"
+
+    rbflw_api_key = input("Enter your API_key: ")
+    rbflw_workspace = input("Enter your workspace: ")
+    rbflw_project = input("Enter your project: ")
+    rbflw_download= input("Enter your Download:")
     rf = Roboflow(api_key=rbflw_api_key)
     project = rf.workspace(rbflw_workspace).project(rbflw_project)
     dataset = project.version(2).download(rbflw_download)
