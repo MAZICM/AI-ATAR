21666c94e84c735eab9d515f9ad63957b6454a2a
otman-dev <mouhib_otm@upf.ac.ma>
2023-08-15 23:21:27 +0100

    Utilities separated :)

---
+++ Utilities/modelTrain.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+
+def mTrain():
+
+    #model_path="/home/kenaro/ForestFireDetection/yolov8m.pt"
+    #data_path="/home/kenaro/ForestFireDetection/AI-Yolo/Wildfire-2/data.yaml"
+    #epochs=4
+    #imgsz=240
+    #device=0
+    #workers=8
+    #project="labelModel"
+    #name="train1"
+
+    model_path = input("Model Path :")
+    data_path = input("Data Path :")
+    epochs = input("Epochs :")
+    imgsz = input("imgsz :")
+    device = input("device :")
+    workers = input("workers :")
+    project = input("project :")
+    name = input("name :")
+
+    model = YOLO(model_path)
+
+    model.train(data=data_path, epochs=epochs, imgsz=imgsz, device=device,
+                workers=workers, project=project, name=name, show_labels=True)
---
+++ Utilities/sDetect.py
@@ -0,0 +1,29 @@
+
+import subprocess
+
+
+
+def Stream():
+    #task1 = "detect"
+    #mode1 = "predict"
+    #model1 = "test/train1/weights/best.pt"
+    #source1 = "0"
+    #device1 = "0"
+    #name1 = "yolov8_StreamTest"
+    task1 = input("task :")
+    mode1 = input("mode :")
+    model1 = input("model :")
+    source1 = input("source :")
+    device1 = input("device :")
+    name1 = input("name :")
+    liveStream = [
+        "yolo",
+        "task=", task1,
+        "mode=", mode1,
+        "model=", model1,
+        "source=", source1,
+        "device=", device1,
+        "name=", name1,
+        "show=", "True",
+    ]
+    subprocess.run(liveStream)
